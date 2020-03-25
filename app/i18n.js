d0e7aaa4dbf34c1766f9a071e83ecbbb4a135ad7
ui_9456dev <5285556-2020_ui_9456dev@users.noreply.gitlab.com>
2020-03-25 18:56:42 +0530

    clear unnecessary files

--- app/i18n.js
+++ app/i18n.js
@@ -7,22 +7,18 @@
  *   script `extract-intl`, and must use CommonJS module syntax
  *   You CANNOT use import/export in this file.
  */
-const addLocaleData = require('react-intl').addLocaleData; //eslint-disable-line
-const enLocaleData = require('react-intl/locale-data/en');
-const deLocaleData = require('react-intl/locale-data/de');
+//const addLocaleData = require('react-intl').addLocaleData; //eslint-disable-line
+//const enLocaleData = require('react-intl/locale-data/en');
 
 const enTranslationMessages = require('./translations/en.json');
-const deTranslationMessages = require('./translations/de.json');
 
-addLocaleData(enLocaleData);
-addLocaleData(deLocaleData);
+//addLocaleData(enLocaleData);
 
 const DEFAULT_LOCALE = 'en';
 
 // prettier-ignore
 const appLocales = [
   'en',
-  'de',
 ];
 
 const formatTranslationMessages = (locale, messages) => {
@@ -42,7 +38,6 @@ const formatTranslationMessages = (locale, messages) => {
 
 const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
-  de: formatTranslationMessages('de', deTranslationMessages),
 };
 
 exports.appLocales = appLocales;
